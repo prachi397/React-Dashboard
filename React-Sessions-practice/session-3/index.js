c4cbb609b0b013176e94a41047e781f018efd396
unknown <prachipanwar397@gmail.com>
2024-09-21 11:20:31 +0530

    pura and impure functions

--- React-Sessions-practice/session-3/index.js
+++ React-Sessions-practice/session-3/index.js
@@ -23,3 +23,55 @@ console.log("Passwords having length greater than 9 (imperative code) :",longPas
 //declarative code
 const longPasswords = passwords.filter((password)=>password.length>=9);
 console.log("Long Passwords using declarative code :", longPasswords);
+
+console.log("==================================================");
+
+// When doing Functional Programming, functions are first-class objects. This means we can use
+// functions as if they were variables or constants.
+// we can combine functions with other functions and generate new functions in the process 
+// ( and higher-order functions) 
+
+const byTwo = function (value) {
+    return value / 2;
+  };
+  
+const ans = byTwo(8);
+console.log(ans);
+
+console.log("==================================================");
+//Pure Function -> A pure function is a function which:
+// If given the same inputs, always returns the same output (immutability)
+// Has no side-effects. In practical scenarios, think of pure functions as functions which
+// return a predictable and deterministic value.
+
+//Pure Function
+function addTwo1(a, b) {
+    return a + b;
+  }
+console.log("pure function => ", addTwo1(2,5));
+//Impure Function
+function addTwo2(a, b) {
+    console.log(a, b);
+  }
+addTwo2(5,2);
+
+console.log("==================================================");
+
+// In FP we avoid declaring any global variables.
+// Instead pass the variables around from within a function to another function.
+
+//Global Variables
+let x = 15;
+let y = 10;
+
+const add = () => {
+  //Impure function using global variables
+  return x + y;
+};
+console.log("Impure function return :",add());
+const addFP = (x, y) => {
+  //Pure function only using what's passed to it
+  return x + y;
+};
+console.log("Pure function return :",addFP(5,8));
+    
